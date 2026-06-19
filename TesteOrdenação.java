ff8366f094ce3f8cdc9d9beedbb32abe354124ac
Bernardo Friske <148400713+I3erfs@users.noreply.github.com>
2026-06-19 14:55:45 -0300

    Create TesteOrdenação.java with initial structure

---
+++ TesteOrdenação.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class ComparaUltimaLetra implements Comparator<String> {
+    @Override
+
+    }
+}
+
+public class TesteOrdenacao {
+    public static void main(String[] args) {
+
+    }
+}
